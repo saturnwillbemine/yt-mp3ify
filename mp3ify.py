4cff99976f91df797d3f3afe00b83d34d7509147
Trey <lewistrejon@gmail.com>
2023-09-28 12:55:52 -0500

    began download_playlist func

--- mp3ify.py
+++ mp3ify.py
@@ -1,8 +1,9 @@
-from pytube import YouTube
+from pytube import YouTube, Playlist
 from pytube.exceptions import VideoUnavailable
 from moviepy.editor import *
 import proglog
 import os
+import re
 
 
 # YOU MUST GO INTO cipher.py of Pytube and change line 411 to 'transform_plan_raw = js'
@@ -11,23 +12,31 @@ import os
 def download_video(link):
         try:
                 video = YouTube(link, use_oauth=True, allow_oauth_cache=True)
+
         except VideoUnavailable:
                 print(f"Video at link:{link} is unavailable!")
+
         else:
                 stream = video.streams.get_highest_resolution()
-                stream.download('./Downloads')
+                stream.download('./Outputs')
                 print(f"{video.title} downloaded successfully!")
-                video_path = f"./Downloads/{stream.default_filename}"
+                video_path = f"./Outputs/{stream.default_filename}"
 
         print(f"Your video will be found at {video_path}")
+
         return video_path, video.title
+
+
+def download_playlist(link):
+        playlist = Playlist(link)
+
 
 
 def convert_to_mp3(mp4_path, mp3_filename):
         file_to_convert = AudioFileClip(mp4_path)
 
         print("Converting to mp3...")
-        file_to_convert.write_audiofile(f'./mp3outputs/{mp3_filename}.mp3',
+        file_to_convert.write_audiofile(f'./Outputs/{mp3_filename}.mp3',
                                         verbose=False, logger=proglog.TqdmProgressBarLogger(print_messages=False))
 
         file_to_convert.close()
@@ -38,5 +47,5 @@ def convert_to_mp3(mp4_path, mp3_filename):
                 print("Path cleanup complete")
 
 
-video = download_video('https://www.youtube.com/watch?v=AuaXmo12hAc')
+video = download_video('https://youtu.be/JWrvMw57slc?si=8Fd5kgip98WXkhCr')
 convert_to_mp3(video[0], video[1])
