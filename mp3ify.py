85b540523c38ac7b6d79c930ad05d8590a2ecaf6
Trey <lewistrejon@gmail.com>
2023-09-28 09:37:27 -0500

    download works, added exceptions

--- mp3ify.py
+++ mp3ify.py
@@ -1,18 +1,21 @@
 from pytube import YouTube
+from pytube.exceptions import VideoUnavailable
 
-video_path = './mp3ify'
+
+# YOU MUST GO INTO cipher.py of Pytube and change line 411 to 'transform_plan_raw = js'
 
 
 def download_video(link):
-    try:
-        video = YouTube(link, use_oauth=True, allow_oauth_cache=True)
-        stream = video.streams.get_highest_resolution()
-        stream.download()
+        try:
+                video = YouTube(link, use_oauth=True, allow_oauth_cache=True)
+        except VideoUnavailable:
+                print(f"Video at link:{link} is unavailable!")
+        else:
+                stream = video.streams.get_highest_resolution()
+                stream.download('./Downloads')
+                print(f"{video.title} downloaded successfully!")
 
-        print(video.title)
-        print(f"Your video will be found at {video_path}")
-    except Exception as e:
-        print(f"Error at {str(e)}")
+        print(f"Your video will be found at ../yt-mp3ify/Downloads")
 
 
-download_video("https://www.youtube.com/watch?v=-zvWBnQ0E2k")
+download_video('https://www.youtube.com/watch?v=AuaXmo12hAc')
